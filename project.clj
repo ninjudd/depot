acc47648979f0cd0123e713fc4477f16d321c938
Anthony Grimes <i@raynes.me>
2011-08-15 18:10:51 -0500

    Take an optional filtering function.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject depot "0.1.3"
+(defproject depot "0.1.4"
   :description "A place for storing goods or motor vehicles."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.4.0"]
+                 [useful "0.6.0"]
                  [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]])
--- src/depot/maven.clj
+++ src/depot/maven.clj
@@ -1,5 +1,6 @@
 (ns depot.maven
-  (:use uncle.core depot.deps)
+  (:use uncle.core depot.deps
+        [useful.map :only [filter-vals]])
   (:import [org.apache.maven.artifact.ant DependenciesTask RemoteRepository InstallTask Pom]
            [org.apache.tools.ant.taskdefs Delete]
            [org.apache.maven.model Dependency Exclusion]
@@ -13,12 +14,12 @@
 (defn- exclusion [dep]
   (ant-type Exclusion {:group-id (namespace dep) :artifact-id (name dep)}))
 
-(defn- dependencies [spec type]
-  (for [[dep opts] (get spec type)]
+(defn- dependencies [deps]
+  (for [[dep opts] deps]
     (ant-type Dependency
       {:group-id    (namespace dep)
        :artifact-id (name dep)
-       :version     (or (:version opts) (get-in spec [:dependencies dep :version]) "LATEST")
+       :version     (or (:version opts) "LATEST")
        :classifier  (:classifier opts)
        :exclusions  (map exclusion (concat *exclusions* (:exclusions opts)))})))
 
@@ -26,8 +27,11 @@
   (doseq [dep deps]
     (.addDependency task dep)))
 
-(defmethod fetch-deps :maven [spec type]
-  (when-let [deps (seq (dependencies spec type))]
+(defmethod fetch-deps :maven [spec & [f]]
+  (when-let [deps (seq (dependencies
+                        (if f
+                          (filter-vals (:dependencies spec) f)
+                          (:dependencies spec))))]
     (ant DependenciesTask {:fileset-id "depot.fileset" :path-id (:name spec)}
       (add-repositories (into *repositories* (:repositories spec)))
       (add-dependencies deps))
