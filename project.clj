50564e41e568fb74e56ff29332dfd4ecc5453752
Justin Balthrop <git@justinbalthrop.com>
2011-09-24 22:49:13 -0700

    resolving transitive dependencies mostly works

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Library for fetching Clojure dependencies."
   :dependencies [[clojure "1.2.0"]
                  [uncle "0.2.3"]
-                 [useful "0.7.0-alpha3"]
+                 [useful "0.7.0-beta4"]
                  [org.clojars.ninjudd/data.xml "0.0.1-SNAPSHOT"]])
--- src/depot/core.clj
+++ src/depot/core.clj
@@ -1,24 +1,53 @@
 (ns depot.core
-  (:use depot.xml
-        [clojure.data.xml :only [lazy-parse]])
+  (:use [depot.xml :only [get-in-xml as-map]]
+        [depot.version :only [newer?]]
+        [clojure.data.xml :only [lazy-parse]]
+        [useful.utils :only [queue]]
+        [useful.map :only [map-keys index-by]])
   (:require [clojure.string :as s]))
 
 (defn group-path [group]
   (s/replace (name group) #"/." "/"))
 
-(defn metadata [repo group artifact]
-  (lazy-parse (format "%s/%s/%s/maven-metadata.xml"
-                      repo (group-path group) artifact)))
+(defn metadata [repo {:keys [group artifact]}]
+  (try (lazy-parse (format "%s/%s/%s/maven-metadata.xml"
+                           repo (group-path group) artifact))
+       (catch Exception e)))
 
-(defn pom [repo group artifact version]
-  (lazy-parse (format "%s/%s/%s/%s/%3$s-%4$s.pom"
-                      repo (group-path group) artifact version)))
+(defn pom [repo {:keys [group artifact version]}]
+  (try (lazy-parse (format "%s/%s/%s/%s/%3$s-%4$s.pom"
+                           repo (group-path group) artifact version))
+       (catch Exception e)))
 
-(defn versions [repo group artifact]
-  (get-in-xml (metadata repo group artifact)
+(defn versions [repo project]
+  (get-in-xml (metadata repo project)
               [:metadata :versioning :versions :version/*]))
 
-(defn dependencies [repo group artifact version]
-  (map as-map
-       (get-in-xml (pom repo group artifact version)
-                   [:project :dependencies :dependency/*])))
+(defn project-key [project]
+  (symbol (:group project) (:artifact project)))
+
+(defn normalize-key [key]
+  (keyword (s/replace (name key) #"Id$" "")))
+
+(defn dependencies [repo project]
+  (index-by project-key
+            (for [dep (get-in-xml (pom repo project)
+                                  [:project :dependencies :dependency/*])]
+              (map-keys (as-map dep) normalize-key))))
+
+(defn resolve-deps [repo project]
+  (loop [projects   {}
+         to-resolve (conj (queue) [(project-key project) project])]
+    (if (empty? to-resolve)
+      projects
+      (let [[key project] (peek to-resolve)
+            to-resolve    (pop  to-resolve)]
+        (if-let [existing (get projects key)]
+          (recur (if (newer? (:version project) (:version existing))
+                   (assoc-in projects [key :version] (:version project))
+                   projects)
+                 to-resolve)
+          (let [deps (dependencies repo project)]
+            (recur (assoc projects key
+                          (assoc project :dependencies deps))
+                   (into to-resolve deps))))))))
