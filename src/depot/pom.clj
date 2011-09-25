62b93a63702f06591592a80a0fe71b370c6d89c5
Justin Balthrop <git@justinbalthrop.com>
2011-09-24 22:49:27 -0700

    renaming

--- src/depot/pom.clj
+++ src/depot/pom.clj
@@ -5,53 +5,53 @@
 (defn pomify [key]
   (->> key name camelize keyword))
 
-(defmulti xml-tags
+(defmulti pom-sexps
   (fn [tag value] (keyword "depot.pom" (name tag))))
 
-(defmethod xml-tags :default
+(defmethod pom-sexps :default
   ([tag value]
      (when value
        [(pomify tag) value])))
 
-(defmethod xml-tags ::list
+(defmethod pom-sexps ::list
   ([tag values]
-     [(pomify tag) (map (partial xml-tags
+     [(pomify tag) (map (partial pom-sexps
                                  (-> tag name (s/replace #"ies$" "y") keyword))
                         values)]))
 
 (doseq [c [::dependencies ::repositories]]
   (derive c ::list))
 
-(defmethod xml-tags ::exclusions
+(defmethod pom-sexps ::exclusions
   [tag values]
   [:exclusions
    (map
     (fn [dep]
-      [:exclusion (map (partial apply xml-tags)
+      [:exclusion (map (partial apply pom-sexps)
                        {:group-id (namespace dep)
                         :artifact-id (name dep)})])
     values)])
 
-(defmethod xml-tags ::dependency
+(defmethod pom-sexps ::dependency
   ([_ [dep opts]]
      [:dependency
-      (map (partial apply xml-tags)
+      (map (partial apply pom-sexps)
            {:group-id    (namespace dep)
             :artifact-id (name dep)
             :version     (:version opts)
             :classifier  (:classifier opts)
             :exclusions  (:exclusions opts)})]))
 
-(defmethod xml-tags ::repository
+(defmethod pom-sexps ::repository
   ([_ [id url]]
      [:repository [:id id] [:url url]]))
 
-(defmethod xml-tags ::project
+(defmethod pom-sexps ::project
   ([tag values]
      (list
       [:project {:xmlns "http://maven.apache.org/POM/4.0.0"}
        [:modelVersion "4.0.0"]
-       (map (partial apply xml-tags)
+       (map (partial apply pom-sexps)
             (select-keys values
                          (rseq
                           [:artifact-id :group-id :version :name
